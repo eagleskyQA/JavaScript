30690278d3c4b7ccd7132ccd9184064dd708f538
eagleskyQA <eagle.skyqa@gmail.com>
2022-06-14 18:51:11 +0300

    fixed some steps

--- JS_HW_1.js
+++ JS_HW_1.js
@@ -18,12 +18,12 @@ let item_5 = item_3;
 let item_6 = 15;
 let item_6_type = typeof(item_6);
 
-console.log(`item_6 = ${typeof (item_6)}, item_6_type = ${typeof (item_6_type)}`);
+console.log(`item_6 = ${item_6} ${typeof (item_6)}, item_6_type = ${item_6_type} ${typeof (item_6_type)}`);
 
 let item_7 = String(item_6);
 let item_7_type = typeof(item_7);
 
-console.log(`item_7 = ${typeof (item_7)}, item_7_type = ${typeof (item_7_type)}`);
+console.log(`item_7 = ${item_7} ${typeof(item_7)}, item_7_type = ${item_7_type} ${typeof(item_7_type)}`);
 
 let age_1 = 10;
 let age_2 = 18;
@@ -31,7 +31,7 @@ let age_3 = 60;
 
 if (age_1 < age_2) {
   console.log('You don’t have access cause your age is ' + age_1 + ' It’s less then');
-} else if (age_1 >= age_2 & age_1 < age_3) {
+} else if (age_1 >= age_2 && age_1 < age_3) {
   console.log('Welcome  !');
 } else if (age_1 > age_3) {
   console.log('Keep calm and look Culture channel');
